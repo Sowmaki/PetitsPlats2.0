65840a08073ae6a78fdccb88d9d7f2fb424a8fef
sowmaki <solene.tassel@hotmail.com>
2024-11-27 18:03:49 +0100

    fonction de recherche de la barre principale achevée, un peu de style.

---
+++ templates/card.js
@@ -0,0 +1,89 @@
+export function recipesTemplate(data) {
+  const results = document.createElement('ul')
+  results.classList.add('results')
+
+  data.forEach(recipe => {
+    // Récupération des données de la recette
+    const imgSrc = `assets/images/${recipe.image}`;
+    const duration = `${recipe.time}`;
+    const title = `${recipe.name}`;
+    const description = `${recipe.description}`;
+    const ingredients = recipe.ingredients;
+
+    // Création de la carte de recette
+    const card = document.createElement('li');
+    card.classList.add('recipe');
+
+    const recipeContainer = document.createElement('div');
+    recipeContainer.classList.add('recipe-container');
+
+    // Partie supérieure de la carte (image et durée)
+    const recipeTop = document.createElement('div');
+    recipeTop.classList.add('recipe-container__top');
+    recipeTop.innerHTML = `
+    <img class="recipe__img" src="${imgSrc}" alt="${title}">
+    <div class="recipe__duration">${duration} min</div>
+  `;
+
+    // Partie inférieure de la carte (titre, description, ingrédients)
+    const recipeBottom = document.createElement('div');
+    recipeBottom.classList.add('recipe-container__bottom');
+
+    // Titre
+    const recipeTitle = document.createElement('h2');
+    recipeTitle.classList.add('recipe__name');
+    recipeTitle.innerText = `${title}`
+
+    // Description
+    const descriptionContainer = document.createElement('div');
+    descriptionContainer.classList.add('recipe-container__description');
+    descriptionContainer.innerHTML = `
+    <h3>Recette</h3>
+    <p class="recipe__description">${description}</p>
+  `;
+
+    // Liste des ingrédients
+    const ingredientsContainer = document.createElement('div');
+    ingredientsContainer.classList.add('recipe-container__ingredients');
+
+    const h3 = document.createElement('h3');
+    h3.innerText = 'Ingrédients';
+
+    const ingredientsList = document.createElement('ul');
+    ingredientsList.classList.add('recipe__ingredients');
+
+    // Boucle pour créer chaque ingrédient
+    ingredients.forEach(element => {
+      const ingredient = document.createElement('li');
+      ingredient.classList.add('recipe__ingredient-item');
+
+      const ingredientName = element.ingredient;
+      const ingredientQuantity = `${element.quantity ? element.quantity : ""} ${element.unit ? element.unit : ""}`;
+
+      ingredient.innerHTML = `
+      <span class="recipe__ingredient">${ingredientName}</span>
+      <span class="recipe__ingredient--quantity">${ingredientQuantity}</span>
+    `;
+      ingredientsList.appendChild(ingredient);
+    });
+
+    // Ajout des éléments au conteneur des ingrédients
+    ingredientsContainer.appendChild(h3);
+    ingredientsContainer.appendChild(ingredientsList);
+
+    // Assemblage des parties de la carte
+    recipeBottom.appendChild(recipeTitle);
+    recipeBottom.appendChild(descriptionContainer);
+    recipeBottom.appendChild(ingredientsContainer);
+    recipeContainer.appendChild(recipeTop);
+    recipeContainer.appendChild(recipeBottom);
+    card.appendChild(recipeContainer);
+
+    // Ajout de la carte au conteneur principal
+    results.appendChild(card);
+
+  })
+
+  return results
+
+}
---
+++ utils/research.js
@@ -0,0 +1,73 @@
+import { recipesTemplate } from '../templates/card.js';
+
+async function getAllRecipes() {
+  try {
+    const response = await fetch('datas/recipes.json')
+    if (!response.ok) {
+      throw new Error('could not fetch data')
+    }
+    const data = await response.json()
+    console.log(data);
+
+    return data
+  }
+  catch (error) {
+    console.error(error);
+  }
+}
+
+const mainResults = document.querySelector(".main-results")
+// Affiche les medias et leurs données après les avoir créé 
+export async function displayRecipesData(selectedRecipes) {
+  ; //recupere l'endroit ou vont etre affichés les medias
+  // supprime l'element medias du main s'il existe deja
+  mainResults.querySelector(".results")?.remove()
+  // Variable contenant une liste créée à partir du template associé
+  const recipesListDOM = recipesTemplate(selectedRecipes);
+  mainResults.appendChild(recipesListDOM);
+}
+
+async function getRecipesFromResearch() {
+  const input = document.getElementById('main-searchbar-input');
+  // stocke tout ce que contient notre recipes.json dans une variable
+  const allRecipes = await getAllRecipes();
+  input.addEventListener('input', () => {
+    // récupère la valeur de l'input de saisie 
+    const inputValue = input.value.toLowerCase()
+    //Si l'input n'a pas le minimum de 3 caractères, ne rien faire
+    // Initialisation du tableau des recettes sélectionnées
+    let selectedRecipes = [];
+
+    // Parcourir toutes les recettes
+    allRecipes.forEach(recipe => {
+
+      // Vérifier si un ingrédient correspond à inputValue
+      const hasMatchingIngredient = recipe.ingredients.some(ingredient =>
+        ingredient.ingredient.toLowerCase().includes(inputValue)
+      );
+
+      // Vérifier si le titre contient inputValue
+      const hasMatchingTitle = recipe.name.toLowerCase().includes(inputValue);
+
+      // Vérifier si la description contient inputValue
+      const hasMatchingDescription = recipe.description.toLowerCase().includes(inputValue);
+
+      // Ajouter la recette à selectedRecipes si au moins une condition est vraie et qu'elle n'y est pas déjà
+      if ((hasMatchingIngredient || hasMatchingTitle || hasMatchingDescription) &&
+        !selectedRecipes.includes(recipe)) {
+        selectedRecipes.push(recipe);
+      }
+    });
+
+    // Afficher les recettes filtrées si le tableau n'est pas vide
+    if (selectedRecipes.length < 3) {
+      console.log('3 caracteres minimum');
+      return
+    } else {
+      displayRecipesData(selectedRecipes);
+    }
+  });
+
+}
+
+getRecipesFromResearch()
