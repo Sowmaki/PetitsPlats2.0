6f95892a13ab9b8adaa162f33e6a9cd1c8bb41bf
sowmaki <solene.tassel@hotmail.com>
2024-11-28 12:47:43 +0100

    fonction affichage des listes dans les dropdowns des filtres terminée.

--- utils/filterResearch.js
+++ utils/filterResearch.js
@@ -9,80 +9,69 @@ const filtersObject = {
     allElements: allRecipes.flatMap(recipe =>
       recipe.ingredients.map(ingredient => ingredient.ingredient)
     ),
-    // hasMatching: recipe.ingredients.some(ingredient =>
-    //   ingredient.ingredient.toLowerCase().includes(inputValue)
-    // ),
+    dropdown: document.getElementById('ingredients-dropdown'),
   },
   appareils: {
     value: 'appareils',
     input: document.getElementById('appareils'),
     allElements: [...new Set(allRecipes.flatMap(recipe => recipe.appliance))],
-    // hasMatching: recipe.appliance.toLowerCase().includes(inputValue),
+    dropdown: document.getElementById('appareils-dropdown'),
   },
   ustensiles: {
     value: 'ustensiles',
     input: document.getElementById('ustensiles'),
     allElements: [...new Set(allRecipes.flatMap(recipe => recipe.appliance))],
-    // hasMatching: recipe.ustensils.toLowerCase().includes(inputValue),
+    dropdown: document.getElementById('ustensiles-dropdown'),
   }
 };
 
 
 Object.entries(filtersObject).forEach(([key, filter]) => {
-  console.log(`Clé : ${key}`);
-  console.log('Valeur :', filter);
 
-  // Exemple : accéder à des propriétés spécifiques
-  console.log(`Champ input pour ${filter.value} :`, filter.input);
-  console.log(`Tous les éléments pour ${filter.value} :`, filter.allElements);
+  function displayFilterList(tab) {
+    const dropdownMenu = filter.dropdown
+    const suggestionsList = document.createElement('ul')
+    suggestionsList.classList.add('advanced__suggestions')
+
+    tab.forEach(element => {
+      const suggestion = document.createElement('li')
+      suggestion.classList.add('suggestion')
+      suggestion.innerText = `${element}`
+      suggestionsList.appendChild(suggestion)
+    })
+    if (!dropdownMenu.querySelector('.advanced__suggestions')) {
+      dropdownMenu.appendChild(suggestionsList)
+    } else {
+      dropdownMenu.querySelector('.advanced__suggestions').remove()
+      dropdownMenu.appendChild(suggestionsList)
+    }
+  }
+
+  const inputFilter = filter.input
+
+  function filtersResearch() {
+    const allElements = filter.allElements
+
+    inputFilter.addEventListener('input', () => {
+      let matchingElements = []
+      const inputValue = inputFilter.value.toLowerCase()
+      // if (inputValue.length < 1) {
+      //   console.log('3 caracteres minimum');
+      //   return
+      // } else {
+      allElements.forEach(element => {
+        if (element.toLowerCase().includes(inputValue)) {
+          !matchingElements.includes(element) && matchingElements.push(element)
+        }
+      })
+      console.log(matchingElements);
+      displayFilterList(matchingElements)
+      // }
+    })
+
+  }
+
+  filtersResearch()
+
 });
 
-
-
-const dropdownMenu = document.querySelector('.advanced__dropdown-menu');
-const inputIngredients = document.getElementById('ingredients')
-let selectedIngredients = []
-
-function displayFilterList(tab) {
-  const suggestionsList = document.createElement('ul')
-  suggestionsList.classList.add('advanced__suggestions')
-
-  tab.forEach(element => {
-    const suggestion = document.createElement('li')
-    suggestion.classList.add('suggestion')
-    suggestion.innerText = `${element}`
-    suggestionsList.appendChild(suggestion)
-  })
-  if (!document.querySelector('.advanced__suggestions')) {
-    dropdownMenu.appendChild(suggestionsList)
-  } else {
-    document.querySelector('.advanced__suggestions').remove()
-    dropdownMenu.appendChild(suggestionsList)
-  }
-}
-
-async function filtersResearch() {
-  const allIngredients = allRecipes.flatMap(recipe =>
-    recipe.ingredients.map(ingredient => ingredient.ingredient)
-  );
-
-  inputIngredients.addEventListener('input', () => {
-    let matchingIngredients = []
-    const inputValue = inputIngredients.value.toLowerCase()
-    // if (inputValue.length < 1) {
-    //   console.log('3 caracteres minimum');
-    //   return
-    // } else {
-    allIngredients.forEach(ingredient => {
-      if (ingredient.toLowerCase().includes(inputValue)) {
-        !matchingIngredients.includes(ingredient) && matchingIngredients.push(ingredient)
-      }
-    })
-    console.log(matchingIngredients);
-    displayFilterList(matchingIngredients)
-    // }
-  })
-
-}
-
-filtersResearch()
