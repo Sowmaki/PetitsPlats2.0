bcbe88f671b47f2d6d75649d39cc17b4c4e3a527
sowmaki <solene.tassel@hotmail.com>
2024-12-05 17:34:29 +0100

    suppression de arrayAll qui sert a rien. On met every a la place. petite modification dans les matchn de labels

--- utils/mainResearch.js
+++ utils/mainResearch.js
@@ -33,11 +33,8 @@ export async function getRecipesFromResearch() {
   const isInputValid = inputValue.length >= 3; // Vérifier si l'input contient au moins 3 caractères
   const allLabels = document.querySelectorAll('.labels__label'); // Récupérer les étiquettes
   const selectedLabels = [...allLabels].map(label => label.innerText.toLowerCase()); // Convertir en tableau de chaînes en minuscules
-
-  // Fonction pour vérifier si un tableau satisfait un prédicat pour TOUS ses éléments
-  function arrayAll(array, predicate) {
-    return array.every(predicate);
-  }
+  const allAppliances = [...new Set(allRecipes.flatMap(recipe => recipe.appliance))];
+  const allUstensils = [...new Set(allRecipes.flatMap(recipe => recipe.ustensils.map(ustensil => ustensil.toLowerCase())))];
 
   // Filtrage des recettes
   const selectedRecipes = allRecipes.filter(recipe => {
@@ -45,30 +42,44 @@ export async function getRecipesFromResearch() {
     const hasMatchingIngredient = recipe.ingredients.some(ingredient => {
       const ingredientName = ingredient.ingredient.toLowerCase();
       const matchesInput = isInputValid ? ingredientName.includes(inputValue) : true; // Match avec l'input (ou non si l'input est vide)
-      const matchesAllLabels = arrayAll(selectedLabels, label => ingredientName.includes(label)); // Match avec toutes les étiquettes
+      const matchesAllLabels = selectedLabels.every(label => ingredientName.includes(label)); // Match avec toutes les étiquettes
       return matchesInput && matchesAllLabels; // Doit satisfaire les deux conditions
     });
 
     // Vérifier le titre
-    const hasMatchingTitle = isInputValid
-      ? recipe.name.toLowerCase().includes(inputValue)
-      : true;
-    const matchesTitleLabels = arrayAll(selectedLabels, label =>
+    // hasMatchingTitle vérifie si le nom de la recette contient la valeur de l'input (en ignorant la casse), sinon considère que c'est vrai.
+    const hasMatchingTitle = isInputValid ? recipe.name.toLowerCase().includes(inputValue) : true;
+    const matchesTitleLabels = selectedLabels.every(label =>
       recipe.name.toLowerCase().includes(label)
     );
     const titleValid = hasMatchingTitle && matchesTitleLabels;
 
     // Vérifier la description
-    const hasMatchingDescription = isInputValid
-      ? recipe.description.toLowerCase().includes(inputValue)
-      : true;
-    const matchesDescriptionLabels = arrayAll(selectedLabels, label =>
+    const hasMatchingDescription = isInputValid ? recipe.description.toLowerCase().includes(inputValue) : true;
+    const matchesDescriptionLabels = selectedLabels.every(label =>
       recipe.description.toLowerCase().includes(label)
     );
     const descriptionValid = hasMatchingDescription && matchesDescriptionLabels;
 
+    //Vérifier les appareils
+    const applianceLabel = selectedLabels.filter(label => allAppliances.some(appliance => appliance.toLowerCase().includes(label)))
+    let hasMatchingAppliance = false
+    if (applianceLabel.length !== 0) {
+      hasMatchingAppliance = applianceLabel.every(label =>
+        recipe.appliance.toLowerCase().includes(label.toLowerCase()))
+    }
+
+    //Vérifier les ustensiles
+    const ustensilLabel = selectedLabels.filter(label => allUstensils.some(ustensil => ustensil.toLowerCase().includes(label)))
+    let hasMatchingUstensil = false;
+
+    if (ustensilLabel.length !== 0) {
+      hasMatchingUstensil = ustensilLabel.every(label =>
+        recipe.appliance.toLowerCase().includes(label.toLowerCase()))
+    }
+
     // Une recette est valide si elle satisfait l'une des trois conditions
-    return hasMatchingIngredient || titleValid || descriptionValid;
+    return hasMatchingIngredient || titleValid || descriptionValid || hasMatchingUstensil || hasMatchingAppliance;
   });
 
   // Afficher les recettes filtrées ou un message si aucune recette n'est trouvée
